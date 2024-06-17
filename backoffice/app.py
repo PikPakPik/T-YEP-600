258f02749b541664cb4c1316715db288b9aa208f
Valentin R <valentin.ragot2001@gmail.com>
2024-06-17 12:39:12 +0000

    YEP-10: Login

--- backoffice/app.py
+++ backoffice/app.py
@@ -1,20 +1,24 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config.update(
     SECRET_KEY = os.environ.get('SECRET_KEY'),
+    JWT_KEY = os.environ.get('JWT_KEY'),
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 from controllers import *
-from models import *
+import services.authorization
 
 db.init_app(app)
+login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
---
+++ backoffice/services/authorization.py
@@ -0,0 +1,18 @@
+import datetime
+from app import app, login_manager
+from models.Session import Session
+
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    authorization_header = request.headers.get('Authorization')
+    if (authorization_header is None) or (authorization_header.startswith('Bearer ') is False):
+        return None
+    
+    token = authorization_header[7:]
+
+    session = Session.query.filter_by(token=token).first()
+    if (session is None) or (session.lastUsedAt <= datetime.datetime.now()):
+        return None
+    
+    return session.user
