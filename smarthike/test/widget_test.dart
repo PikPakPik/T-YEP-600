3b4c5d7776562683d95126da5c681f8f1f373e93
Alexandre Tressel <12072176-atressel@users.noreply.gitlab.com>
2024-06-18 08:24:22 +0000

    :sparkles: [FRONT]feat(19): add navigation menu

--- smarthike/test/widget_test.dart
+++ smarthike/test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:smarthike/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('NavigationBarApp has 3 navigation destinations',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NavigationBarApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that NavigationBarApp has 3 navigation destinations.
+    expect(find.byType(NavigationDestination), findsNWidgets(3));
   });
 }
