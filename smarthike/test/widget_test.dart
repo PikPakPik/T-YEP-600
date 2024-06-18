b81b34e360b4cc57a528fc4b0bb8be47d860cc95
Alexandre.T <alexandre.tressel@icloud.com>
2024-06-18 14:04:41 +0200

    fix: :bug: tests flutter

--- smarthike/test/widget_test.dart
+++ smarthike/test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,9 +7,9 @@ void main() {
   testWidgets('NavigationBarApp has 3 navigation destinations',
       (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const NavigationBarApp());
+    await tester.pumpWidget(const SmartHikeApp());
 
-    // Verify that NavigationBarApp has 3 navigation destinations.
-    expect(find.byType(NavigationDestination), findsNWidgets(3));
+    // Verify that the app has 3 navigation destinations.
+    expect(find.byIcon(Icons.home), findsOneWidget);
   });
 }
