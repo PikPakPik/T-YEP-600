ed916cb3a619f3388b37a0b0060112ec5b17ae8d
Valentin R <valentin.ragot2001@gmail.com>
2024-07-08 12:13:00 +0000

    Fix command flask import:hikes

--- backoffice/commands/import_hikes.py
+++ backoffice/commands/import_hikes.py
@@ -1,12 +1,12 @@
 from app import app, db
+from models.Hike import Hike
 import click
 import os
 import pandas as pd
-from models.Hike import Hike
 
 @app.cli.command('import:hikes')
 @click.argument('filename', required=True)
-def hello(filename=None):
+def importHikes(filename=None):
     filename = './csv/' + filename
 
     if not os.path.exists(filename):
@@ -14,32 +14,28 @@ def hello(filename=None):
         return
     
     try:
+        batchSize = 1000
+        batchCounter = 0
         df = pd.read_csv(filename, delimiter=';')
         df = df.where(pd.notnull(df), None)
 
-        expected_columns = {'FID', 'osm_id', 'name'}
-        if not expected_columns.issubset(df.columns):
-            return
-
         for index, row in df.iterrows():
-            try:
-                if row['FID'] is None or row['osm_id'] is None:
-                    print(f"Missing required field in row {index}: {row.to_dict()}")
+            new_hike = Hike(
+                osmId = int(row['osm_id']),
+                name = row['name'] if row['name'] is not None else f"Hike {index}"
+            )
+            db.session.add(new_hike)
+            
+            if batchCounter % batchSize == 0:
+                db.session.commit()
+                print(f"Processed {batchCounter} records")
 
-                new_hike = Hike(
-                    osmId=int(row['osm_id']),
-                    name=row['name'],
-                )
-                db.session.add(new_hike)
-            except Exception as e:
-                db.session.rollback()
-                print(f"Error adding row {index}: {row.to_dict()}")
-                print(f"Exception: {e}")
+            batchCounter += 1
 
         db.session.commit()
         print('Data imported successfully')
-        return
 
     except Exception as e:
+        db.session.rollback()
         print(f"Exception during processing: {e}")
         return
